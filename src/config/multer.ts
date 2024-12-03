591ad9d6b572a0c6abf339285a4bd9c5dbc88610
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-12-03 05:06:09 +0000

    teste 19

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,19 +1,45 @@
-import crypto from 'crypto'
-import multer from 'multer'
-import { extname, resolve } from 'path'
+// import crypto from 'crypto'
+// import multer from 'multer'
+// import { extname, resolve } from 'path'
+
+// export default {
+//     upload(folder: string) {
+//         return {
+//             storage: multer.diskStorage({
+//                 destination: resolve(__dirname, '..', '..', folder),
+//                 filename: (request, file, callback) => {
+//                     const fileHash = crypto.randomBytes(16).toString('hex')
+//                     const filename = `${fileHash}-${file.originalname}`
+
+//                     return callback(null, filename)
+//                 }
+//             })
+//         }
+//     }
+// }
+
+import crypto from 'crypto';
+import multer from 'multer';
+import { extname, resolve } from 'path';
 
 export default {
-    upload(folder: string) {
-        return {
-            storage: multer.diskStorage({
-                destination: resolve(__dirname, '..', '..', folder),
-                filename: (request, file, callback) => {
-                    const fileHash = crypto.randomBytes(16).toString('hex')
-                    const filename = `${fileHash}-${file.originalname}`
+  upload(folder: string) {
+    return {
+      storage: multer.diskStorage({
+        destination: (req, file, callback) => {
+          const destinationPath = process.env.NODE_ENV === 'production'
+            ? '/tmp' // Em produção, use a pasta temporária
+            : resolve(__dirname, '..', '..', folder); // Localmente, use a pasta configurada
 
-                    return callback(null, filename)
-                }
-            })
-        }
-    }
-}
+          callback(null, destinationPath);
+        },
+        filename: (req, file, callback) => {
+          const fileHash = crypto.randomBytes(16).toString('hex');
+          const filename = `${fileHash}-${file.originalname}`;
+
+          callback(null, filename);
+        },
+      }),
+    };
+  },
+};
