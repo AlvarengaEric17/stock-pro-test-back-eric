945c9fde66e0d589d0556515bdf6b99e0a2fceda
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-12-03 05:24:50 +0000

    teste 20

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,45 +1,19 @@
-// import crypto from 'crypto'
-// import multer from 'multer'
-// import { extname, resolve } from 'path'
-
-// export default {
-//     upload(folder: string) {
-//         return {
-//             storage: multer.diskStorage({
-//                 destination: resolve(__dirname, '..', '..', folder),
-//                 filename: (request, file, callback) => {
-//                     const fileHash = crypto.randomBytes(16).toString('hex')
-//                     const filename = `${fileHash}-${file.originalname}`
-
-//                     return callback(null, filename)
-//                 }
-//             })
-//         }
-//     }
-// }
-
-import crypto from 'crypto';
-import multer from 'multer';
-import { extname, resolve } from 'path';
+import crypto from 'crypto'
+import multer from 'multer'
+import { extname, resolve } from 'path'
 
 export default {
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: (req, file, callback) => {
-          const destinationPath = process.env.NODE_ENV === 'production'
-            ? '/tmp' // Em produção, use a pasta temporária
-            : resolve(__dirname, '..', '..', folder); // Localmente, use a pasta configurada
+    upload(folder: string) {
+        return {
+            storage: multer.diskStorage({
+                destination: resolve(__dirname, '..', '..', folder),
+                filename: (request, file, callback) => {
+                    const fileHash = crypto.randomBytes(16).toString('hex')
+                    const filename = `${fileHash}-${file.originalname}`
 
-          callback(null, destinationPath);
-        },
-        filename: (req, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString('hex');
-          const filename = `${fileHash}-${file.originalname}`;
-
-          callback(null, filename);
-        },
-      }),
-    };
-  },
-};
+                    return callback(null, filename)
+                }
+            })
+        }
+    }
+}
