c6be4847736bceed9b9b6c03fa159ca52a67ea52
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-12-03 05:33:52 +0000

    teste 21

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,17 +1,37 @@
+// import crypto from 'crypto'
+// import multer from 'multer'
+// import { extname, resolve } from 'path'
+
+// export default {
+//     upload(folder: string) {
+//         return {
+//             storage: multer.diskStorage({
+//                 destination: resolve(__dirname, '..', '..', folder),
+//                 filename: (request, file, callback) => {
+//                     const fileHash = crypto.randomBytes(16).toString('hex')
+//                     const filename = `${fileHash}-${file.originalname}`
+
+//                     return callback(null, filename)
+//                 }
+//             })
+//         }
+//     }
+// }
+
 import crypto from 'crypto'
 import multer from 'multer'
-import { extname, resolve } from 'path'
+import { resolve } from 'path'
 
 export default {
     upload(folder: string) {
         return {
             storage: multer.diskStorage({
-                destination: resolve(__dirname, '..', '..', folder),
+                // Alterando para o diretório temporário '/tmp'
+                destination: resolve('/tmp', folder),
                 filename: (request, file, callback) => {
                     const fileHash = crypto.randomBytes(16).toString('hex')
                     const filename = `${fileHash}-${file.originalname}`
-
-                    return callback(null, filename)
+                    callback(null, filename)
                 }
             })
         }
