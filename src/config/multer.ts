6d1e9da61e848fc1e48437fc7cac86da07fc34b4
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2025-02-13 18:12:37 +0000

    teste 24

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -24,58 +24,6 @@
 
 
 
-// import crypto from 'crypto'
-// import multer from 'multer'
-// import { resolve } from 'path'
-
-// export default {
-//     upload(folder: string) {
-//         return {
-//             storage: multer.diskStorage({
-//                 // Alterando para o diretório temporário '/tmp'
-//                 destination: resolve('/tmp', folder),
-//                 filename: (request, file, callback) => {
-//                     const fileHash = crypto.randomBytes(16).toString('hex')
-//                     const filename = `${fileHash}-${file.originalname}`
-//                     callback(null, filename)
-//                 }
-//             })
-//         }
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import crypto from 'crypto'
 import multer from 'multer'
 import { resolve } from 'path'
@@ -84,7 +32,8 @@ export default {
     upload(folder: string) {
         return {
             storage: multer.diskStorage({
-                destination: resolve(__dirname, '..', 'tmp', folder), // Corrigindo o caminho para a pasta tmp dentro do projeto
+                // Alterando para o diretório temporário '/tmp'
+                destination: resolve('/tmp', folder),
                 filename: (request, file, callback) => {
                     const fileHash = crypto.randomBytes(16).toString('hex')
                     const filename = `${fileHash}-${file.originalname}`
@@ -94,3 +43,19 @@ export default {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,82 @@
+// import express, { Request, Response, NextFunction } from "express";
+// import 'express-async-errors';
+// import cors from "cors";
+// import path from 'path'
+
+// import { router } from "./routes";
+
+// const app = express();
+// app.use(express.json())
+// app.use(cors())
+
+// app.use(router)
+
+// app.use(
+//     '/files',
+//     express.static(path.resolve(__dirname, '..', 'tmp'))
+// )
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//     if (err instanceof Error) {
+//         return res.status(400).json({
+//             error: err.message
+//         })
+//     }
+//     return res.status(500).json({
+//         status: 'error',
+//         message: 'Internal server erro'
+//     })
+// })
+// export default app
+
+// app.listen(3333, () => console.log("SERVIDOR RODANDO ⏳"))
+
+// // deu erro 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import express, { Request, Response, NextFunction } from "express";
 import 'express-async-errors';
 import cors from "cors";
-import path from 'path'
+import path from 'path';
 
 import { router } from "./routes";
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use(router)
+app.use(router);
 
-app.use(
-    '/files',
-    express.static(path.resolve(__dirname, '..', 'tmp'))
-)
+// Servir arquivos da pasta /tmp corretamente
+app.use('/files', express.static('/tmp'));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
         return res.status(400).json({
             error: err.message
-        })
+        });
     }
     return res.status(500).json({
         status: 'error',
-        message: 'Internal server erro'
-    })
-})
-export default app
+        message: 'Internal server error'
+    });
+});
 
-app.listen(3333, () => console.log("SERVIDOR RODANDO ⏳"))
+app.listen(3333, () => console.log("SERVIDOR RODANDO ⏳"));
 
-// deu erro 
+export default app;
