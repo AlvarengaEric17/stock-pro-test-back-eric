837b5211557a5ce67df5455f167ef2b7dc30f8c7
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2025-02-13 17:58:46 +0000

    teste

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -1,37 +1,75 @@
+// // import crypto from 'crypto'
+// // import multer from 'multer'
+// // import { extname, resolve } from 'path'
+
+// // export default {
+// //     upload(folder: string) {
+// //         return {
+// //             storage: multer.diskStorage({
+// //                 destination: resolve(__dirname, '..', '..', folder),
+// //                 filename: (request, file, callback) => {
+// //                     const fileHash = crypto.randomBytes(16).toString('hex')
+// //                     const filename = `${fileHash}-${file.originalname}`
+
+// //                     return callback(null, filename)
+// //                 }
+// //             })
+// //         }
+// //     }
+// // }
+
 // import crypto from 'crypto'
 // import multer from 'multer'
-// import { extname, resolve } from 'path'
+// import { resolve } from 'path'
 
 // export default {
 //     upload(folder: string) {
 //         return {
 //             storage: multer.diskStorage({
-//                 destination: resolve(__dirname, '..', '..', folder),
+//                 // Alterando para o diretório temporário '/tmp'
+//                 destination: resolve('/tmp', folder),
 //                 filename: (request, file, callback) => {
 //                     const fileHash = crypto.randomBytes(16).toString('hex')
 //                     const filename = `${fileHash}-${file.originalname}`
-
-//                     return callback(null, filename)
+//                     callback(null, filename)
 //                 }
 //             })
 //         }
 //     }
 // }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import crypto from 'crypto'
 import multer from 'multer'
-import { resolve } from 'path'
+import { extname, resolve } from 'path'
 
 export default {
     upload(folder: string) {
         return {
             storage: multer.diskStorage({
-                // Alterando para o diretório temporário '/tmp'
-                destination: resolve('/tmp', folder),
+                destination: resolve(__dirname, '..', '..', folder),
                 filename: (request, file, callback) => {
                     const fileHash = crypto.randomBytes(16).toString('hex')
                     const filename = `${fileHash}-${file.originalname}`
-                    callback(null, filename)
+
+                    return callback(null, filename)
                 }
             })
         }
